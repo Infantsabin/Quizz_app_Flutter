b713bedb7bd3bb3b49afd0939ceb75972679972d
Infant Sabin <arockiamson@gmail.com>
2020-05-15 11:34:30 +0530

    README changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+//import 'package:quizzler/question.dart';
+import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +30,13 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekepper = [];
+//  List<String> questions = [
+////    'You can lead a cow down stairs but not up stairs.',
+////    'Approximately one quarter of human bones are in the feet.',
+////    'A slug\'s blood is green.',
+////  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +73,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.getCorrectAnswer();
+                if (correctAnswer == true) {
+                  print("You're right");
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
                 //The user picked true.
               },
             ),
@@ -79,10 +98,20 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.getCorrectAnswer();
+                if (correctAnswer == false) {
+                  print("You're right");
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
                 //The user picked false.
               },
             ),
           ),
+        ),
+        Row(
+          children: scorekepper
         ),
         //TODO: Add a Row here as your score keeper
       ],
